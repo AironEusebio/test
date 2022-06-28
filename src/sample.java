a0b93464c837b547eb6cfab3fae7070974e26450
AironEusebio <aironeusebio@gmail.com>
2022-06-28 14:44:57 +0800

    added a method
    String name method

--- src/sample.java
+++ src/sample.java
@@ -2,4 +2,8 @@ public class sample {
     public static void main(String args[]){
         System.out.println("kasjd");
     }
+
+    static String name(String name){
+        return name;
+    }
 }
